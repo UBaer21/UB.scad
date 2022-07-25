dfce777da481a6715c3a794c30f21f43a831938f
Ulrich Bär <93376232+UBaer21@users.noreply.github.com>
2022-07-25 22:24:10 +0200

    Delete UBscadDoku/html directory

--- UBscadDoku/html/dir_a5836f5f599914ba8b66facd3124d330.js
+++
@@ -1,4 +0,0 @@
-var dir_a5836f5f599914ba8b66facd3124d330 =
-[
-    [ "ub22.146Doxy.scad", "ub22_8146_doxy_8scad.html", "ub22_8146_doxy_8scad" ]
-];
--- UBscadDoku/html/files_dup.js
+++
@@ -1,4 +0,0 @@
-var files_dup =
-[
-    [ "UBscadDoku", "dir_a5836f5f599914ba8b66facd3124d330.html", "dir_a5836f5f599914ba8b66facd3124d330" ]
-];
